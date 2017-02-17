ee0c1e7bf0202e397caf02e09e9fbdec086f923d
powmonk <dan@aldron.co.uk>
2017-02-17 17:21:36 +0000

    So many changes...

--- Java_II_ass_III/src/TileGame.java
+++ Java_II_ass_III/src/TileGame.java
@@ -1,17 +1,12 @@
 import java.awt.event.ActionListener;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.swing.JButton;
-import javax.swing.JToggleButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
-
 import java.awt.event.ActionEvent;
-import java.awt.TextField;
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
 
@@ -20,7 +15,6 @@ public class TileGame {
 		TileFrame frame = new TileFrame();
 		frame.setTitle("Tile Game");
 		frame.setVisible(true);
-
 	}
 	
 	static class TileFrame extends JFrame implements ActionListener{
@@ -43,6 +37,7 @@ public class TileGame {
 		}
 		
 		private void addFooter() {
+			//ignore this comment
 			footerPanel = new JPanel();
 			footerPanel.setLayout(new GridLayout());
 			
@@ -60,17 +55,18 @@ public class TileGame {
 		}
 
 		private void addButtons() {
-			for(int i=0;i<4;i++){
+			//The outer loop creates the rows, the inner loops creates the columns
+			for(int i=0;i<N;i++){
 				JPnls.add(i, new JPanel());
 				JPnls.get(i).setLayout(new GridLayout(1,4));
 				
-				for(int j=0; j<4; j++){
-					JBtns[i][j] = new JToggleButton();
+				for(int j=0; j<N; j++){
+					//Here the buttons are created by using the inner and outer loop counter for X + Y positions
+					JBtns[i][j] = new JButton();
 					JBtns[i][j].addActionListener(this);
 					JPnls.get(i).add(JBtns[i][j]);
 				}
 			}
-			
 			setButtons();
 		}
 
@@ -78,14 +74,11 @@ public class TileGame {
 			int count = 0;
 			for(int i=0; i<N;i++){
 				for(int j=0; j<N;j++){
-					
+					//The loop counters give X+Y positions to the 2D array
 					JBtns[i][j].setText(tiles[count]);
 					count++;
-
 				}
-
 			}
-
 		}
 
 		private void randomise(){
@@ -107,23 +100,19 @@ public class TileGame {
 		}
 		
 		private void swapNumbers(ActionEvent e){
-			
 			prev = curr;
-			
+			//this loop compares the tiles array to the command value of the button to check if a number and assign it to curr
 			for(int i=0;i<tiles.length;i++){
-				if(tiles[i] == e.getActionCommand().toString() && e.getActionCommand() != " "){
+				if(tiles[i] == e.getActionCommand().toString() ){
 					curr = i;
 					footerText.setText("");
 				}
 			}
 
-			if(prev < 0 && tiles[curr] != ""){
-				clearToggle();
-				footerText.setText("Start by selecting the empty tile");
-				prev = -1;
-				curr = -1;
-			}
-			
+			// if(prev > -1 && tiles[curr] != ""){
+			// 	footerText.setText("No!");
+			// } 
+				
 			if(prev > -1 && curr > -1){
 				temp = tiles[prev];
 				tiles[prev] = tiles[curr];
@@ -131,37 +120,86 @@ public class TileGame {
 				setButtons();
 				prev = -1;
 				curr = -1;
-				clearToggle();
+				resetButtons();
 			}
-			
-			
 		}
 
-		private void clearToggle() {
+		private void resetButtons() {
 			for(int i=0;i<N;i++){
 				for(int j=0;j<N;j++){
+					JBtns[i][j].setBackground(null);
 					JBtns[i][j].setSelected(false);
 				}
 			}
 		}
 
-		private void checkButton(ActionEvent e) {
-			for(int i=0; i<N; i++){
-				for(int j=0; j<N; j++){
-					e.getActionCommand();
 
+		private void setTiles(ActionEvent e){
+			for(int i=0;i<N;i++){
+				for(int j=0;j<N;j++){
+					if(JBtns[i][j] == e.getSource()){
+						JBtns[i][j].setBackground(activeTile);
+					}
+				}
+			}
+		}
+
+		private static boolean isValidMove(Object e) {
+			for(int i=0;i<N;i++){
+				for(int j=0;j<N;j++){
+					if(JBtns[i][j].getBackground() == activeTile){
+ 					}
+
+					if(JBtns[i][j].getActionCommand() == ""){
+						if(JBtns[i][j] == e){return true;}
+						if(i-1 > -1){if(JBtns[i-1][j] == e){return true;};}
+						if(i+1 <  N){if(JBtns[i+1][j] == e){return true;};}
+						if(j-1 > -1){if(JBtns[i][j-1] == e){return true;};}
+						if(j+1 <  N){if(JBtns[i][j+1] == e){return true;};}
+					}
 				}
 			}
 			
+			return false;
 		}
 		
 		private static boolean winCondition(){
+			int parsed;
 			for(int i=0; i<tiles.length-1; i++){
-				if(Integer.parseInt(tiles[i]) != i+1){
+				// This try-catch stops the contents of the empty tile being compared to a number
+				try{
+					parsed = Integer.parseInt(tiles[i]);
+				}catch(NumberFormatException e) { 
+			    	return false; 
+			    }catch(NullPointerException e) {
+			    	return false;
+			    }
+
+				if(parsed != i+1){
 					return false;
 				}
 			}
 			return true;
+		}
+		
+		private void rainbowTiles(){
+			int count = 0;
+			try{
+				while(true){
+					count=count>255?0:count;
+					
+					for(int i=0;i<N;i++){
+						for(int j=0;j<N;j++){
+							JBtns[i][i].setBackground(new Color(count, count, count));
+						}
+					}
+					count++;
+					Thread.sleep(10000);
+				}
+				
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}
 		}
 		
 		@Override
@@ -171,30 +209,36 @@ public class TileGame {
 				footerText.setText(" ");
 				randomise();
 				setButtons();
-				clearToggle();
+				resetButtons();
 			}else{
-//				footerText.setText(e.toString());
-				swapNumbers(e);
 				
-				checkButton(e);
+				if(isValidMove(e.getSource())){
+					setTiles(e);
+					swapNumbers(e);
+					rainbowTiles();
+
+				}else{
+					footerText.setText("You need to pick a tile adjacent to the empty tile");
+				}
 				
+				//Check if  the game has been beaten
 				if(winCondition()){
+					rainbowTiles();
 					footerText.setText("WIN!!!");
 				}
 			}
-			
 		}
 		
-
+	private static Color activeTile = new Color(200,200,255);
+	private static Object selected, prevSelected;
 	private static final int N = 4;
 	private static String[] tiles = new String[N*N];	
 	private JPanel footerPanel;
 	private JButton shuffleButton;
 	private List<JPanel>  JPnls = new ArrayList<JPanel>();
-	private JToggleButton[][] JBtns = new JToggleButton[N][N];
-//	private List<JButton> JBtns = new ArrayList<JButton>();
+	private static JButton[][] JBtns = new JButton[N][N];
 	private JTextArea footerText;
-	private int prev = -1, curr = -1;
+	private static int prev = -1, curr = -1;
 	private String temp;
 	}
 }
